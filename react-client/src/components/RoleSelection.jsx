5d5b92728be1464d95458f39a7cdfe10ffbf0882
KevinLiu <kevinbondliu@gmail.com>
2017-07-06 11:29:55 -0700

    Implemented User and Instructor Role Selection

---
+++ react-client/src/components/RoleSelection.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class RoleSelection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: ''
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // componentDidMount() {
+  //   console.log(this.props, 'didmount');
+  // }
+
+  handleClick(event) {
+    event.preventDefault();
+    this.props.changeRoles(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="col-xs-12 text-center">Select Your Role!
+        <div>
+          <span>
+            <button className='student' value = {'STUDENT'} onClick = {(event) => {this.handleClick(event)}}>Student</button>
+            <button className='instructor' value = {'INSTRUCTOR'} onClick = {(event) => {this.handleClick(event)}}>instructor</button>
+          </span>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RoleSelection;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -4,270 +4,282 @@ import $ from 'jquery';
 import Login from './components/Login.jsx';
 import Student from './components/Student.jsx';
 import Instructor from './components/Instructor.jsx';
+import RoleSelection from './components/RoleSelection.jsx'
 import axios from 'axios';
-
 const io = require('socket.io-client');
 const socket = io();
 
 var countdownInterval;
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      view: '',
-      tokenId: '',
-      lectureStatus: 'lectureNotStarted',
-      lectureId: '',
-      questionId: '',
-      thumbValue: 2,
-      countdown: 30,
-      givenName: '',
-      lectureName: 'lobby',
-      roomChange: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            view: '',
+            tokenId: '',
+            lectureStatus: 'lectureNotStarted',
+            lectureId: '',
+            questionId: '',
+            thumbValue: 2,
+            countdown: 30,
+            givenName: '',
+            lectureName: 'lobby',
+            roomChange: ''
+        }
+        this.role;
     }
-  }
 
-  componentDidMount() {
-    this.setState({ view: 'login' });
-  }
+    componentDidMount() {
+        this.setState({ view: 'roleSelect' });
+    }
 
-  onSignIn(googleUser) {
-    let tokenId = googleUser.tokenId;
-    axios({
-      method: 'get',
-      url: '/googleLogin',
-      params: {
-        tokenId: tokenId
-      }
-    })
-    .then(result => {
+    onSignIn(googleUser) {
+        let tokenId = googleUser.tokenId;
+        axios({
+            method: 'get',
+            url: '/googleLogin',
+            params: {
+                tokenId: tokenId,
+                role: this.role
+            }
+        })
+            .then(result => {
 
-      socket.emit('lectureName', {room: 'lobby'});
-      if (result.data[0].user_type === 'STUDENT') {
-        this.setState({ view: 'student'});
-      } else if (result.data[0].user_type === 'INSTRUCTOR') {
-        this.setState({ view: 'instructor'});
-      }
-      this.setState({ givenName: googleUser.profileObj.givenName })
-      socket.emit('username', { username: googleUser.profileObj.email })
-      if(result.data[0].user_type === 'INSTRUCTOR'){
-        socket.emit('instructor', { username: googleUser.profileObj.email })
-      }
-    });
+                socket.emit('lectureName', { room: 'lobby' });
+                if (result.data[0].user_type === 'STUDENT') {
+                    this.setState({ view: 'student' });
+                } else if (result.data[0].user_type === 'INSTRUCTOR') {
+                    this.setState({ view: 'instructor' });
+                }
+                this.setState({ givenName: googleUser.profileObj.givenName })
+                socket.emit('username', { username: googleUser.profileObj.email })
+                if (result.data[0].user_type === 'INSTRUCTOR') {
+                    socket.emit('instructor', { username: googleUser.profileObj.email })
+                }
+            });
 
-  }
+    }
 
-  onFBSignIn(facebookUser) {
-    var nameArray = facebookUser.name.split(' ');
-    axios({
-      method: 'get',
-      url: '/FBLogin',
-      params: {
-        firstName: nameArray[0],
-        lastName: nameArray[1],
-        email: facebookUser.email
-      }
-    })
-      .then(result => {
-        console.log('this is the result from fb', result);
-        if (result.data[0].user_type === 'STUDENT') {
-          this.setState({ view: 'student' });
-        } else if (result.data[0].user_type === 'INSTRUCTOR') {
-          this.setState({ view: 'instructor' });
+    onFBSignIn(facebookUser) {
+        var nameArray = facebookUser.name.split(' ');
+        axios({
+            method: 'get',
+            url: '/FBLogin',
+            params: {
+                firstName: nameArray[0],
+                lastName: nameArray[1],
+                email: facebookUser.email,
+                role: this.role
+            }
+        })
+            .then(result => {
+                console.log('this is the result from fb', result);
+                if (result.data[0].user_type === 'STUDENT') {
+                    this.setState({ view: 'student' });
+                } else if (result.data[0].user_type === 'INSTRUCTOR') {
+                    this.setState({ view: 'instructor' });
+                }
+                this.setState({ givenName: facebookUser.name })
+                socket.emit('username', { username: facebookUser.email })
+                if (result.data[0].user_type === 'INSTRUCTOR') {
+                    socket.emit('instructor', { username: facebookUser.email })
+                }
+            })
+    }
+
+    startLecture(lectureId, lectureName) {
+        this.setState({
+            lectureStatus: 'lectureStarted',
+            lectureId: lectureId,
+            lectureName: lectureName
+        })
+    }
+
+    endLecture() {
+        let lectureId = this.state.lectureId;
+        console.log(lectureId);
+        axios({
+            method: 'post',
+            url: '/endLecture',
+            params: {
+                lectureId: lectureId
+            }
+        }).then((result) => {
+            this.setState({
+                lectureStatus: 'lectureNotStarted',
+                lectureId: ''
+            })
+        })
+    }
+
+    endLectureStudent() {
+        this.setState({
+            lectureStatus: 'lectureNotStarted'
+        })
+    }
+
+    setCountdownInterval() {
+        countdownInterval = setInterval(() => {
+            this.state.countdown === 0
+                ? this.clearCountdownInterval()
+                : this.setState({ countdown: this.state.countdown - 1 }, () => {
+                    console.log('this.state.countdown', this.state.countdown);
+                    if (this.state.view === 'student') {
+                        socket.emit('thumbValue', { thumbValue: this.state.thumbValue });
+                    }
+                });
+        }, 1000)
+    }
+
+    clearCountdownInterval() {
+        clearInterval(countdownInterval);
+        if (this.state.view === 'student') {
+            this.setState({
+                lectureStatus: 'lectureStarted',
+                questionId: '',
+                countdown: 30
+            })
         }
-        this.setState({ givenName: facebookUser.name })
-        socket.emit('username', { username: facebookUser.email })
-        if (result.data[0].user_type === 'INSTRUCTOR') {
-          socket.emit('instructor', { username: facebookUser.email })
-        }
-      })
-  }
+    }
 
-  startLecture(lectureId, lectureName) {
-    this.setState({
-      lectureStatus: 'lectureStarted',
-      lectureId: lectureId,
-      lectureName: lectureName
-    })
-  }
+    startThumbsCheck(questionId) {
+        this.setState({
+            lectureStatus: 'checkingThumbs',
+            questionId: questionId
+        }, this.setCountdownInterval)
+    }
 
-  endLecture() {
-    let lectureId = this.state.lectureId;
-    console.log(lectureId);
-    axios({
-      method: 'post',
-      url: '/endLecture',
-      params: {
-        lectureId: lectureId
-      }
-    }).then((result) => {
-      this.setState({
-        lectureStatus: 'lectureNotStarted',
-        lectureId: ''
-      })
-    })
-  }
+    endThumbsCheck() {
+        this.setState({
+            lectureStatus: 'lectureStarted',
+            questionId: ''
+        })
+    }
 
-  endLectureStudent() {
-    this.setState({
-      lectureStatus: 'lectureNotStarted'
-    })
-  }
+    clearThumbsCheck() {
+        this.setState({
+            lectureStatus: 'lectureStarted',
+            questionId: '',
+            countdown: 30
+        })
+    }
 
-  setCountdownInterval() {
-    countdownInterval = setInterval(() => {
-      this.state.countdown === 0
-        ? this.clearCountdownInterval()
-        : this.setState({ countdown: this.state.countdown - 1 }, () => {
-          console.log('this.state.countdown', this.state.countdown);
-          if (this.state.view === 'student') {
-            socket.emit('thumbValue', { thumbValue: this.state.thumbValue });
-          }
+    changeThumbValue(value) {
+        this.setState({
+            thumbValue: value
+        })
+    }
+    signOut() {
+        // FB.logout(function(response){
+        //   console.lg
+        // })
+        var auth2 = gapi.auth2.getAuthInstance();
+        console.log(auth2);
+        auth2.signOut().then(function() {
+            console.log('User signed out.');
         });
-    }, 1000)
-  }
+        console.log(this.state.view);
+        this.setState({
+            view: 'roleSelect'
+        })
+    }
 
-  clearCountdownInterval() {
-    clearInterval(countdownInterval);
-    if (this.state.view === 'student') {
+    changeLecture(lectureName) {
+        event.preventDefault();
+        axios({
+            method: 'post',
+            url: '/checkLectures',
+            params: {
+                lectureName: lectureName
+            }
+        }).then(result => {
+            if (result.data === 1) {
+                //console.log('Succesfully found lecture');
+                socket.emit('changeLecture', { currentLecture: this.state.lectureName, newLecture: lectureName });
+                this.setState({ lectureName: lectureName });
+            } else {
+                //console.log('Lecture Not Found');
+            }
+        })
+
+    }
+    handleLectureChange(event) {
+        event.preventDefault();
+        this.changeLecture(this.input.value);
+    }
+
+    changeRoles(role) {
+      this.role = role;
+      console.log(this.role);
       this.setState({
-        lectureStatus: 'lectureStarted',
-        questionId: '',
-        countdown: 30
+        view: 'login'
       })
     }
-  }
 
-  startThumbsCheck(questionId) {
-    this.setState({
-      lectureStatus: 'checkingThumbs',
-      questionId: questionId
-    }, this.setCountdownInterval)
-  }
-
-  endThumbsCheck() {
-    this.setState({
-      lectureStatus: 'lectureStarted',
-      questionId: ''
-    })
-  }
-
-  clearThumbsCheck() {
-    this.setState({
-      lectureStatus: 'lectureStarted',
-      questionId: '',
-      countdown: 30
-    })
-  }
-
-  changeThumbValue(value) {
-    this.setState({
-      thumbValue: value
-    })
-  }
-  signOut() {
-    // FB.logout(function(response){
-    //   console.lg
-    // })
-    var auth2 = gapi.auth2.getAuthInstance();
-    console.log(auth2);
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-    console.log(this.state.view);
-    this.setState({
-      view: 'login'
-    })
-  }
-
-  changeLecture (lectureName) {
-    event.preventDefault();
-    axios({
-      method: 'post',
-      url: '/checkLectures',
-      params: {
-        lectureName: lectureName
-      }
-    }).then(result => {
-      if(result.data === 1){
-        //console.log('Succesfully found lecture');
-        socket.emit('changeLecture', {currentLecture: this.state.lectureName, newLecture: lectureName});
-        this.setState({lectureName: lectureName});
-      } else {
-        //console.log('Lecture Not Found');
-      }
-    })
-    
-    
-
-  }
-  handleLectureChange(event) {
-    event.preventDefault();
-    this.changeLecture(this.input.value);
-  }
-
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-default navbar-static-top">
-          <div className="container-fluid">
-            <button href="#" onClick={this.signOut.bind(this)}>
-              LogOut!
-          </button>
-
-           <form onSubmit={this.handleLectureChange.bind(this)}>
-              Lecture Name:
-              <input type="text" ref={(input) => this.input = input} />
-            <input className="location-submit" type="submit" value="Search for Lecture"/>
-          </form>
-           <div>{this.state.lectureName}</div>
-            <div className="navbar-header">
-              <a className="navbar-brand">
-                <i className="fa fa-thumbs-o-up" aria-hidden="true"></i>
-                &nbsp; Thumblr
-              </a>
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-default navbar-static-top">
+                    <div className="container-fluid">
+                        <button href="#" onClick={this.signOut.bind(this)}>
+                            LogOut!
+                        </button>
+                        <form onSubmit={this.handleLectureChange.bind(this)}>
+                            Lecture Name:
+                            <input type="text" ref={(input) => this.input = input} />
+                            <input className="location-submit" type="submit" value="Search for Lecture" />
+                        </form>
+                        <div>{this.state.lectureName}</div>
+                        <div className="navbar-header">
+                            <a className="navbar-brand">
+                                <i className="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                                &nbsp; Thumblr
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+                <div className="container-fluid main">
+                    {this.state.view === 'roleSelect' ?
+                        <RoleSelection
+                          changeRoles = {this.changeRoles.bind(this)}
+                        />
+                        : this.state.view === 'login'
+                            ? <Login
+                                onSignIn={this.onSignIn.bind(this)}
+                                onFBSignIn={this.onFBSignIn.bind(this)}
+                            />
+                            : this.state.view === 'student'
+                                ? <Student
+                                    thumbValue={this.state.thumbValue}
+                                    changeThumbValue={this.changeThumbValue.bind(this)}
+                                    startThumbsCheck={this.startThumbsCheck.bind(this)}
+                                    startLecture={this.startLecture.bind(this)}
+                                    lectureStatus={this.state.lectureStatus}
+                                    countdown={this.state.countdown}
+                                    view={this.state.view}
+                                    endLectureStudent={this.endLectureStudent.bind(this)}
+                                    givenName={this.state.givenName}
+                                    lectureName={this.state.lectureName}
+                                />
+                                : <Instructor
+                                    thumbValue={this.state.thumbValue}
+                                    lectureId={this.state.lectureId}
+                                    lectureStatus={this.state.lectureStatus}
+                                    startLecture={this.startLecture.bind(this)}
+                                    endLecture={this.endLecture.bind(this)}
+                                    startThumbsCheck={this.startThumbsCheck.bind(this)}
+                                    countdown={this.state.countdown}
+                                    changeThumbValue={this.changeThumbValue.bind(this)}
+                                    clearThumbsCheck={this.clearThumbsCheck.bind(this)}
+                                    view={this.state.view}
+                                    givenName={this.state.givenName}
+                                    lectureName={this.state.lectureName}
+                                />}
+                </div>
             </div>
-          </div>
-        </nav>
-        <div className="container-fluid main">
-          {this.state.view === 'login'
-            ? <Login
-              onSignIn={this.onSignIn.bind(this)}
-              onFBSignIn={this.onFBSignIn.bind(this)}
-            />
-            : this.state.view === 'student'
-              ? <Student
-                thumbValue={this.state.thumbValue}
-                changeThumbValue={this.changeThumbValue.bind(this)}
-                startThumbsCheck={this.startThumbsCheck.bind(this)}
-                startLecture={this.startLecture.bind(this)}
-                lectureStatus={this.state.lectureStatus}
-                countdown={this.state.countdown}
-                view={this.state.view}
-                endLectureStudent={this.endLectureStudent.bind(this)}
-                givenName={this.state.givenName}
-                lectureName={this.state.lectureName}
-              />
-              : <Instructor
-                thumbValue={this.state.thumbValue}
-                lectureId={this.state.lectureId}
-                lectureStatus={this.state.lectureStatus}
-                startLecture={this.startLecture.bind(this)}
-                endLecture={this.endLecture.bind(this)}
-                startThumbsCheck={this.startThumbsCheck.bind(this)}
-                countdown={this.state.countdown}
-                changeThumbValue={this.changeThumbValue.bind(this)}
-                clearThumbsCheck={this.clearThumbsCheck.bind(this)}
-                view={this.state.view}
-                givenName={this.state.givenName}
-                lectureName={this.state.lectureName}
-              />}
-        </div>
-      </div>
 
-    )
-  }
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
