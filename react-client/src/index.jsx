1f8115cc29fa0270b2fc7127a41280336bd978e9
Jerizon <JesseLowellReyes@gmail.com>
2017-07-07 20:35:41 -0700

    Revert to default

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -134,7 +134,7 @@ class App extends React.Component {
                         socket.emit('thumbValue', { thumbValue: this.state.thumbValue });
                     }
                 });
-        }, 500)
+        }, 1000)
     }
 
     clearCountdownInterval() {
